93a1882de5e0e5cb08420f0923e87909207a2af0
CSEugene <cuongnm265@gmail.com>
2016-07-06 01:44:24 +0700

    Styling login page and add some jquery for animation

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,26 +1,10 @@
 class Users::SessionsController < Devise::SessionsController
-# before_action :configure_sign_in_params, only: [:create]
+   respond_to :js
 
-  # GET /resource/sign_in
   # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  #   respond_to do |format|
+  #     format.js
+  #   end
   # end
 
   def create
@@ -30,9 +14,21 @@ class Users::SessionsController < Devise::SessionsController
       set_flash_message(:notice, :signed_in) if is_flashing_format?
       sign_in(resource_name, resource)
       yield resource if block_given?
-      respond_with resource, location: after_sign_in_path_for(resource)
+      # respond_with resource, location: after_sign_in_path_for(resource)
+
+      render :js => "window.location = '/'" and return
     else
-      redirect_to login_path, notice: 'Invalid user or email'
+      # redirect_to login_path, notice: 'Invalid user or email'
+      # render :json => {:success => false, :data => resource.errors.full_messages}
+      # set_flash_message(:alert, :invalid)
+      # render js: flash[:alert], status: 401
+      # respond_to :js
+      respond_to do |format|
+        flash[:notice] = 'Invalid Email or Password'
+        format.js {}
+        format.html { return login_path}
+
+      end
     end
   end
 end
